52ccce6ce991a2fd318060a33f92fbfef4972dcf
Bryan Teo <55470468+BryanTeoTY@users.noreply.github.com>
2019-11-27 19:51:38 +0800

    movefilestofolder
    moveclassfiletofolder

---
+++ TetrisTest/UnitTest2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using NUnit.Framework;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TetrisTest
+{
+    [TestFixture()]
+    public class UnitTest2
+    {
+        [Test()]
+        public void MutingTest()
+        {
+            Assert.AreEqual(false,)
+
+        }
+    }
+}
---
+++ TetrisVideoGame/Globalvariable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace TetrisVideoGame
+{
+    public static class GlobalVariable
+    {
+        public static string image = "image/background_1.jpg";
+        public static bool mute = false;
+    }
+}
